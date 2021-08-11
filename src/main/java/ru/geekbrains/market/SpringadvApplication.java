09e3dbfa281c2d81b14c58d04c99b73be698b4ec
growka <thegusev@gmail.com>
2021-08-11 17:51:00 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/market/SpringadvApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringadvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringadvApplication.class, args);
+    }
+
+}
